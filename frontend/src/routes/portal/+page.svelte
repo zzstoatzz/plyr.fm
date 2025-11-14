796124c9a34713de2ec2d8f3ba4995a7c0b5a50f
nate nowack <thrast36@gmail.com>
2025-11-14 03:29:03 -0600

    fix: display album title instead of [object Object] in portal track list (#234)
    also:
    - change delete button from × to trash icon for better UX

--- frontend/src/routes/portal/+page.svelte
+++ frontend/src/routes/portal/+page.svelte
@@ -663,7 +663,7 @@
 										{/if}
 										{#if track.album}
 											<span class="separator">•</span>
-											<span class="album">{track.album}</span>
+											<span class="album">{track.album.title}</span>
 										{/if}
 									</div>
 									{#if track.created_at}
@@ -685,7 +685,12 @@
 										onclick={() => deleteTrack(track.id, track.title)}
 										title="delete track"
 									>
-										×
+										<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+											<polyline points="3 6 5 6 21 6"></polyline>
+											<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+											<line x1="10" y1="11" x2="10" y2="17"></line>
+											<line x1="14" y1="11" x2="14" y2="17"></line>
+										</svg>
 									</button>
 								</div>
 							{/if}
